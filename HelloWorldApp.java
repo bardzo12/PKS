1f4ee00d45db483b328f43eebbd0cca0b08a4edf
ASUS <ASUS@172.16.122.31>
2015-03-27 23:06:16 +0100

    first

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,3 +1,4 @@
+//first chance
 public class HelloWorldApp {
     
     /** Creates a new instance of HelloWorldApp */
@@ -8,7 +9,7 @@ public class HelloWorldApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello Worldo"); // Display the string.
     }
     
 }
